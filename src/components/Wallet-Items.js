0b032fc06755a12216bbddffc13de2a59fb0e4b8
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-11-20 19:42:15 -0300

    adicionado saldo ao projeto

--- src/components/Wallet-Items.js
+++ src/components/Wallet-Items.js
@@ -3,22 +3,62 @@ import styled from 'styled-components';
 import WalletItem from './Wallet-Item';
 
 export default function WalletItems({ items }) {
+  const sumItems = items.reduce((acc, item) => {
+    if(item.type === 'wallet-in') {
+      acc += item.value
+    }else {
+      acc -= item.value;
+    }
+    return acc;
+  }, 0);
+  const classBalance = sumItems > 0 ? 'positive' : 'negative';
   return (
-    <WalletItemsStyle>
-      {
-        items.map((item, index) => {
-          return (
-            <WalletItem key={index} item={item}/>
-          )
-        })
-      }
-    </WalletItemsStyle>
+    <MainWalletStyle>
+      <WalletItemsStyle>
+        {
+          items.map((item, index) => {
+            return (
+              <WalletItem key={index} item={item}/>
+            )
+          })
+        }
+      </WalletItemsStyle>
+      <WalletBalanceStyle>
+        <span className="label">Saldo</span>
+        <span className={`sum ${classBalance}`}>{sumItems}</span>
+      </WalletBalanceStyle>
+    </MainWalletStyle>
   )
 }
 
+
+const MainWalletStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 23px 12px;
+  height: 100%;
+  justify-content: space-between;
+`
+
 const WalletItemsStyle = styled.ul`
-display: flex;
-flex-direction: column;
-padding: 23px 12px;
-overflow-y: scroll;
+  overflow-y: scroll;
+`
+
+const WalletBalanceStyle = styled.div`
+  position: relative;
+  bottom: 0;
+  display: flex;
+  justify-content: space-between;
+  .label {
+    font-weight: bold;
+    color: #000;
+  }
+  .sum {
+    &.negative {
+      color: #C70000;
+    }
+    &.positve {
+      color: #03AC00;
+    }
+  }
 `
