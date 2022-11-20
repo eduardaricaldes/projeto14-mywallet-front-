e50d47da04b73cf23ac65e93778431860ef8dcbe
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-11-20 18:41:47 -0300

    integrado cash in com o back

--- src/pages/Cash-in.js
+++ src/pages/Cash-in.js
@@ -1,16 +1,61 @@
+import { useState, useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AutenticacaoContext } from '../contexts/AuthenticationProvider';
+import axios from 'axios';
 import styled from 'styled-components';
 
+import { urls } from '../configs/urls';
+
 export default function CashIn (){
+  const navigate = useNavigate();
+  const [token] = useContext(AutenticacaoContext);
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
+
+  function onChangeDescription(value) {
+    if(value !== "") {
+      setDescription(value)
+    }
+  }
+
+  function onChangeValue(value) {
+    if(value !== "") {
+      setValue(value)
+    }
+  }
+
+  function submit(e) {
+    e.preventDefault();
+
+    if(value !== "" && description !== "") {
+      axios.post(urls.wallet, {
+        value: parseFloat(value),
+        description,
+        type: "wallet-in",
+      }, {
+        headers: {
+          Authorization: token,
+        }
+      }).then((resp) => {
+        alert("Nova entrada cadastrada com sucesso")
+        navigate("/home")
+      }).catch((err) => {
+        console.error(err)
+        alert("Erro ao enviar essa entrada")
+      })
+    }
+  }
+
   return(
     <StyledCashIn>
     <StyledHeader>
       <h1 className="title">Nova Entrada</h1>
     </StyledHeader>
       <div className="container">
-        <form className="form">
-        <input type="text" placeholder="Valor"/>
-        <input type="text" placeholder="Descrição"/>
-        <button className="create">Salvar Entrada</button>
+        <form className="form" onSubmit={submit}>
+          <input type="text" placeholder="Valor" onChange={e => onChangeValue(e.target.value)}/>
+          <input type="text" placeholder="Descrição" onChange={e => onChangeDescription(e.target.value)}/>
+          <button className="create">Salvar Entrada</button>
         </form>
       </div>
   </StyledCashIn>
